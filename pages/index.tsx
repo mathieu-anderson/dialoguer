50862dbcfa17ec4607d23efdf32a0609970db876
Mathieu Anderson <anderson@talon.one>
2020-12-28 12:45:29 +0100

    Basic ReactFlow lib integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,45 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import ReactFlow, {
+  addEdge,
+  Background,
+  BackgroundVariant,
+  Connection,
+  Edge,
+  Elements,
+} from 'react-flow-renderer';
+import { useState } from 'react';
+
+const initialElements: Elements = [];
 
 export default function Home() {
+  const [elements, setElements] = useState(initialElements);
+  const nextId = String(elements.length + 1);
+
+  const handleConnect = (connection: Edge | Connection) => {
+    setElements((elements) => addEdge(connection, elements));
+  };
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Dialoguer</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <nav></nav>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Dialoguer</h1>
-        <p className={styles.description}>An app to create dialogue trees</p>
-      </main>
+      <button
+        onClick={() =>
+          setElements([
+            ...elements,
+            {
+              id: nextId,
+              data: { label: `Node ${nextId}` },
+              position: { x: 300, y: 5 },
+              connectable: true,
+            },
+          ])
+        }
+      >
+        Add node
+      </button>
+      <ReactFlow elements={elements} onConnect={handleConnect}>
+        <Background variant={'dots' as BackgroundVariant} gap={20} size={1} />
+      </ReactFlow>
     </div>
   );
 }
